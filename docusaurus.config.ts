75833854018f6c52333310281f0c5f100ccab6c7
Burak <47524842+DevBD1@users.noreply.github.com>
2025-08-30 01:06:33 +0300

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: "https://devbd1.github.io",
+  url: "http://dev.cublex.net", // "https://devbd1.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs-minecraft-plugins/",
+  baseUrl: "/", // /docs-minecraft-plugins
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
